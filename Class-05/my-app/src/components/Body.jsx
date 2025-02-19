a87314399738ce00d49b0ea059457a4d89f2096d
Talha Hayat <taqimadakhail9999@gmail.com>
2025-02-19 23:19:22 +0500

    first commit

---
+++ Class-05/my-app/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import Cards from "./Card";
+import "./Body.css"
+
+export function Body(){
+    return( 
+        <div className="Card-Div">
+            <Cards />
+            <Cards />
+            <Cards />
+        </div>
+    )
+}
