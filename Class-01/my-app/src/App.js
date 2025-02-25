6bd3f537111740cd89a920c8db0dda49fc44e03b
Talha Hayat <taqimadakhail9999@gmail.com>
2025-02-26 00:47:45 +0500

    work complete

---
+++ Class-01/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from './Button.js';
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ Class-01/my-app/src/Button.js
@@ -0,0 +1,6 @@
+
+export function Button(){
+    return(
+        <Button className="Button">Check</Button>
+    )
+}
