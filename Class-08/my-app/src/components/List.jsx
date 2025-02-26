a15738dc77d3a26087069ecdfcbed82ffab45fb6
Talha Hayat <taqimadakhail9999@gmail.com>
2025-02-27 00:30:11 +0500

    Using map method

---
+++ Class-08/my-app/src/components/List.jsx
@@ -0,0 +1,79 @@
+export function List(){
+
+
+
+      const data = [
+    {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+    }
+    },
+    {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+    "street": "Victor Plains",
+    "suite": "Suite 879",
+    "city": "Wisokyburgh",
+    "zipcode": "90566-7771",
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+    "name": "Deckow-Crist",
+    "catchPhrase": "Proactive didactic contingency",
+    "bs": "synergize scalable supply-chains"
+    }
+    },
+    {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+    "street": "Douglas Extension",
+    "suite": "Suite 847",
+    "city": "McKenziehaven",
+    "zipcode": "59590-4157",
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+    "name": "Romaguera-Jacobson",
+    "catchPhrase": "Face to face bifurcated interface",
+    "bs": "e-enable strategic applications"
+    }
+    },
+    ]
+
+    return(
+        <div>
+            <ul>
+            {data.map((value,index)=>{
+                // console.log(value.name)
+                return(
+                <li key={index}>
+                    {value.name}
+                </li>
+                )
+            })}
+            </ul>
+        </div>
+    )
+}
